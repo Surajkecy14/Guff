cd55266505fe6efd471350cb044cb2e6ff8843d1
Surajkecy14 <147683761+Surajkecy14@users.noreply.github.com>
2025-06-26 18:13:47 +0545

    Initial commit

---
+++ client/src/App.js
@@ -0,0 +1,60 @@
+import React, { useEffect,useMemo,useState} from 'react'
+import {io} from 'socket.io-client'
+const App = () => {
+  const [message, setMessage] = useState('');
+  const [roomId,setRoomId]= useState('');
+  const [messages,setMessages]=useState([]);
+  const [id,setId] = useState("");
+  const socket = useMemo( ()=>
+      io("http://localhost:5000")
+  ,[])
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    socket.emit("message",{message,roomId})
+    setMessage(''); 
+  };
+  useEffect(()=>{
+    socket.on("connect", ()=>{
+      setId(socket.id)
+      console.log("connected",socket.id);
+    })
+    socket.on("recieved-message",(m)=>{
+      setMessages((messages)=> [...messages,m])
+    })
+     // eslint-disable-next-line
+  },[])
+   
+  return ( 
+    <div>
+      <h1>HELLO PROGRAMMER!!</h1>
+      <h6>{id}</h6>
+       <form onSubmit={handleSubmit}>
+        <input
+          type="text"
+          placeholder="Write your message..."
+          value={message}
+          onChange={(e) => setMessage(e.target.value)}
+          style={{ padding: '8px', marginRight: '10px' }}
+        />
+         <input
+          type="text"
+          placeholder="room id..."
+          value={roomId}
+          onChange={(e) => setRoomId(e.target.value)}
+          style={{ padding: '8px', marginRight: '10px' }}
+        />
+        <button type="submit" style={{ padding: '8px 16px' }}>Send</button>
+      </form>
+      <div>
+        {
+          messages.map((m,i)=>
+            <li key={i}>{m}</li>
+          )
+        }
+      </div>
+    </div>
+  )
+}
+export default App
+
---
+++ server/index.js
@@ -0,0 +1,33 @@
+import express from 'express'
+import { Server } from 'socket.io';
+import {createServer} from 'http'
+import cors from 'cors'
+
+
+const app = express();
+app.use(cors())
+const server = createServer(app)
+const io = new Server(server,{
+    cors: {
+        origin: "*",
+        credentials:true,
+        methods: ["GET","POST"]
+    }
+})
+const port =5000; 
+
+ 
+app.get('/',(req,res)=>{
+    res.send("hello developer")
+}) 
+
+io.on("connection",(socket)=>{
+    console.log("user connected sucessfully",socket.id)
+    socket.on("message",(data)=>{
+        socket.to(data.roomId).emit("recieved-message",data.message)
+    })
+    })
+  
+server.listen(port,()=>{ 
+    console.log(`app runing at http://localhost ${port}`)
+}) 
